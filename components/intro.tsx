bf546ca7cab611829353ec5deafe1cb45f9b57ca
HafizMohsinRaza <razach495@gmail.com>
2025-05-10 20:48:32 +0500

    testing

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,7 +62,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Shafqat.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Shafqat.</span> I am a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">5+ year</span> of experience as a
         Freelancer. I enjoy building{" "}
